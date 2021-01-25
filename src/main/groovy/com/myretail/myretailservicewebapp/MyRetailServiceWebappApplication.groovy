de3d58464191eec41fad3efa2b117abf54cfb2d2
Sowjanya Tirumala <sowjanyatirumala@outlook.com>
2021-01-25 11:55:13 -0600

    Initial commit

---
+++ src/main/groovy/com/myretail/myretailservicewebapp/MyRetailServiceWebappApplication.groovy
@@ -0,0 +1,13 @@
+package com.myretail.myretailservicewebapp
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MyRetailServiceWebappApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MyRetailServiceWebappApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/myretail/myretailservicewebapp/MyRetailServiceWebappApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.myretail.myretailservicewebapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyRetailServiceWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
