457fd298c70ff483a5f90cf95df8f1b7e05aedaf
Sowjanya Tirumala <sowjanyatirumala@outlook.com>
2021-01-27 11:48:27 -0600

    Added API documentation for the response attributes

--- src/main/groovy/com/myretail/myretailservicewebapp/domain/Product.groovy
+++ src/main/groovy/com/myretail/myretailservicewebapp/domain/Product.groovy
@@ -1,8 +1,15 @@
 package com.myretail.myretailservicewebapp.domain
 
+import io.swagger.annotations.ApiModelProperty
+
 class Product {
+    @ApiModelProperty(notes = 'Product ID', required = true)
     Long id
+
+    @ApiModelProperty(notes = 'Name of the product')
     String name
+
+    @ApiModelProperty(notes = 'Price of the product (with currency code)')
     CurrentPrice currentPrice
 
     Product() {
